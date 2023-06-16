5594e4a868bc30a2e433b4efb3a03236e1dfd254
StellaCea <stela.ceaicovscaia@hotmail.com>
2023-06-16 20:19:56 +0200

    updated index.js by integrating atatus

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('9fa8d242554c437c96db87819d018296').install();
 
 render(
   <React.StrictMode>
