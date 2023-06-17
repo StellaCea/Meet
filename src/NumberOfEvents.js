7eff293a452b3988ce8043b234c7b67139fee741
StellaCea <stela.ceaicovscaia@hotmail.com>
2023-06-17 12:44:56 +0200

    implement Alert

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,17 +1,25 @@
 import React, { Component } from "react";
+import { ErrorAlert } from "./Alert";
 
 class NumberOfEvents extends Component {
     state = {
         query: 32,
+        errorText:""
     };
 
     handleInputChanged = async (event) => {
         const value = event.target.value;
-        if(value >= 1 || value <= 32) {
+        if(value >= 1 && value <= 32) {
             this.setState({
                 query: value,
+                errorText: "",
             });
             this.props.updateEvents(this.props.selectedCity, value);
+        } else{
+            this.setState({
+                query: value,
+                errorText: "Please enter a valid number"
+            });
         }
     };
 
@@ -26,6 +34,7 @@ class NumberOfEvents extends Component {
                     value={this.state.query}
                     onChange={this.handleInputChanged}
                 />
+                <ErrorAlert text={this.state.errorText} />
             </div>
         );
     }
