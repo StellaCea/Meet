91b5c386178cfdfd84c2fb536e3e5033cfd5dfc7
StellaCea <stela.ceaicovscaia@hotmail.com>
2023-06-21 20:54:39 +0200

    Updated WelcomeScreen.css

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import './WelcomeScreen.css';
+import logo from "./meet-logo.png";
 
 function WelcomeScreen(props) {
     return props.showWelcomeScreen ?
         (
             <div className="WelcomeScreen">
+                <div class="welcome_container"></div>
+                <img src={logo}  alt="meet-app-logo" width="300"/>
                 <h1>Welcome to the Meet App</h1>
                 <h4>
                     Log in to see upcoming events around the world for full-stack developers
                 </h4>
                 <div className="button_cont" align="center">
                     <div class="google-btn">
-                        <div class="google.icon-wrapper">
+                        <div class="google-icon-wrapper">
                             <img
                                 class="google-icon"
-                                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="Google sign-in" />
+                                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" 
+                                alt="Google sign-in" 
+                            />
                         </div>
                         <button onClick={() => {props.getAccessToken() }}
                             rel="nofollow opener"
